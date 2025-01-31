bd20f1eec99e9bacabafec0fc1090e79e2c7f798
Andrew Nikolic <a.nikolic@outlook.com>
2025-01-31 11:31:57 +1100

    Fix selections display

--- app.py
+++ app.py
@@ -53,68 +53,69 @@ def update_home_tab(client, event, logger):
             # User is in builder mode, show builder view
             update_app_home_to_builder_mode(client, user_id, app_installed_team_id)
             return
+        else:
 
-        # Retrieve the builder options from the database
-        builder_options = get_user_selections(user_id, app_installed_team_id)  
+            # Retrieve the builder options from the database
+            builder_options = get_user_selections(user_id, app_installed_team_id)  
 
-        # Path to home_tab.json Block Kit template
-        file_path = os.path.join("block_kit", "home_tab.json")
-        
-        # Read the home tab view JSON from the file
-        with open(file_path, "r") as file:
-            view = json.load(file)
+            # Path to home_tab.json Block Kit template
+            file_path = os.path.join("block_kit", "home_tab.json")
+            
+            # Read the home tab view JSON from the file
+            with open(file_path, "r") as file:
+                view = json.load(file)
 
-        # Mapping dictionary
-        option_mapping = {
-            "option-convo":"*Conversations*",
-            "option-channels": "*Channels*",
-            "option-users": "*Users*",
-            "option-canvas": "*Canvas*",
-            "option-apps": "*Apps*"
-        }
+            # Mapping dictionary
+            option_mapping = {
+                "option-convo":"*Conversations*",
+                "option-channels": "*Channels*",
+                "option-users": "*Users*",
+                "option-canvas": "*Canvas*",
+                "option-apps": "*Apps*"
+            }
 
-        # Modify the Block Kit JSON to display builder options
-        if builder_options:
-            selected_options = builder_options.get('multi_static_select-action', [])
+            # Modify the Block Kit JSON to display builder options
+            if builder_options:
+                selected_options = builder_options.get('multi_static_select-action', [])
 
-            # Map the selected values to their display names
-            display_values = [option_mapping.get(value, value) for value in selected_options]
-                
-            if display_values:
-                # Format the selected options into a string
-                options_str = ", ".join(display_values)
-                # Update the Block Kit view with the selected options
-                view["blocks"][3]["elements"] = [
-                    {
-                        "type": "mrkdwn",
-                        "text": f":eight_spoked_asterisk: Current custom demo config: {options_str}"
-                    }
-                ]
+                # Map the selected values to their display names
+                display_values = [option_mapping.get(value, value) for value in selected_options]
+                    
+                if display_values:
+                    # Format the selected options into a string
+                    options_str = ", ".join(display_values)
+                    # Update the Block Kit view with the selected options
+                    view["blocks"][3]["elements"] = [
+                        {
+                            "type": "mrkdwn",
+                            "text": f":eight_spoked_asterisk: Current custom demo config: {options_str}"
+                        }
+                    ]
+                else:
+                    # Display a message when no options are selected
+                    view["blocks"][3]["elements"] = [
+                        {
+                            "type": "mrkdwn",
+                            "text": ":no_entry_sign: Current custom demo config: *No selections.*"
+                        }
+                    ]
             else:
-                # Display a message when no options are selected
+                # Handle case where there are no builder options in the database
                 view["blocks"][3]["elements"] = [
                     {
                         "type": "mrkdwn",
                         "text": ":no_entry_sign: Current custom demo config: *No selections.*"
                     }
                 ]
-        else:
-            # Handle case where there are no builder options in the database
-            view["blocks"][3]["elements"] = [
-                {
-                    "type": "mrkdwn",
-                    "text": ":no_entry_sign: Current custom demo config: *No selections.*"
-                }
-            ]
 
-        # Publish the updated view to the Slack app home
-        client.views_publish(
-            user_id=event["user"],  # User ID from the event
-            view=view
-        )
-        
-        # Log the successful update
-        logger.info(f"Home tab updated for user {user_id}")
+            # Publish the updated view to the Slack app home
+            client.views_publish(
+                user_id=event["user"],  # User ID from the event
+                view=view
+            )
+            
+            # Log the successful update
+            logger.info(f"Home tab updated for user {user_id}")
 
     except Exception as e:
         logger.error(f"Error updating home tab: {e}")
@@ -127,8 +128,6 @@ def handle_enter_builder_mode(ack, body, client, mode):
         # Extract app_installed_team_id
         app_installed_team_id = body["view"].get("app_installed_team_id")
 
-        # Extract app_installed_team_id and user_id
-        app_installed_team_id = body["view"].get("app_installed_team_id")
         user_id = body["user"]["id"]
 
         query = text("""
@@ -462,7 +461,7 @@ def test_connection():
         logger.error(f"Failed to connect to the database: {e}")
         return False
 
-def save_user_selections(user_id, app_installed_team_id, selections, mode='builder'):
+def save_user_selections(user_id, app_installed_team_id, selections):
 
     if not test_connection():
         logger.error("Aborting: Unable to connect to the database.")
@@ -470,13 +469,12 @@ def save_user_selections(user_id, app_installed_team_id, selections, mode='build
     
     try:
         query = text("""
-            INSERT INTO user_builder_selections (user_id, builder_options, last_updated, app_installed_team_id, mode)
-            VALUES (:user_id, :builder_options, :last_updated, :app_installed_team_id, :mode)
+            INSERT INTO user_builder_selections (user_id, builder_options, last_updated, app_installed_team_id)
+            VALUES (:user_id, :builder_options, :last_updated, :app_installed_team_id)
             ON CONFLICT (user_id, app_installed_team_id) 
             DO UPDATE SET 
                 builder_options = EXCLUDED.builder_options,
                 last_updated = EXCLUDED.last_updated
-                mode = EXCLUDED.mode
         """)
         #logger.debug(f"Executing query for user_id {user_id}: {selections}")
         
@@ -485,8 +483,7 @@ def save_user_selections(user_id, app_installed_team_id, selections, mode='build
                 "user_id": user_id,
                 "builder_options": json.dumps(selections),  # Convert selections to JSON string
                 "last_updated": datetime.now(timezone.utc),  # Use timezone-aware datetime
-                "app_installed_team_id": app_installed_team_id,
-                "mode": mode 
+                "app_installed_team_id": app_installed_team_id
             })
             conn.commit()
         logger.debug(f"Successfully saved selections for user_id {user_id}")
@@ -496,12 +493,12 @@ def save_user_selections(user_id, app_installed_team_id, selections, mode='build
 # Retrieve "builder mode" users selections
 def get_user_selections(user_id, app_installed_team_id):
     try:
-        query = text("SELECT builder_options, mode FROM user_builder_selections WHERE user_id = :user_id AND app_installed_team_id = :app_installed_team_id")
+        query = text("SELECT builder_options FROM user_builder_selections WHERE user_id = :user_id AND app_installed_team_id = :app_installed_team_id")
         with engine.connect() as conn:
             result = conn.execute(query, {"user_id": user_id, "app_installed_team_id": app_installed_team_id}).fetchone()
             logger.info(f"Query result for user {user_id} in team {app_installed_team_id}: {result}")
             if result and result[0]:
-                return {"builder_options": result[0], "mode": result[1]}
+                return result[0]
             return None
     except Exception as e:
         logger.error(f"Error getting user selections: {e}")
