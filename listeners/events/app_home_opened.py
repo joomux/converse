a7f10508472666eb7ef785a8045c2981ce1f3514
Andrew Nikolic <a.nikolic@outlook.com>
2025-05-12 13:23:17 +1000

    Fixing app home loading

--- listeners/events/app_home_opened.py
+++ listeners/events/app_home_opened.py
@@ -41,52 +41,70 @@ def app_home_opened_callback(client: WebClient, event: dict, logger: Logger):
             with open(file_path, "r") as file:
                 view = json.load(file)
 
-            # Mapping dictionary
-            option_mapping = {
-                "option-convo":"*Conversations*",
-                "option-channels": "*Channels*",
-                "option-canvas": "*Canvas*",
-                "option-apps": "*Apps*"
-            }
+            # # Mapping dictionary
+            # option_mapping = {
+            #     "option-convo":"*Conversations*",
+            #     "option-channels": "*Channels*",
+            #     "option-canvas": "*Canvas*",
+            #     "option-apps": "*Apps*"
+            # }
 
-            # Modify the Block Kit JSON to display builder options
-            if builder_options:
-                selected_options = builder_options.get('save_builder_config', [])
+            # # Modify the Block Kit JSON to display builder options
+            # if builder_options:
+            #     selected_options = builder_options.get('save_builder_config', [])
 
-                # Map the selected values to their display names
-                display_values = [option_mapping.get(value, value) for value in selected_options]
+            #     # Map the selected values to their display names
+            #     display_values = [option_mapping.get(value, value) for value in selected_options]
                     
-                if display_values:
-                    # Format the selected options into a string
-                    options_str = ", ".join(display_values)
-                    # Update the Block Kit view with the selected options
-                    view["blocks"][3]["elements"] = [
-                        {
-                            "type": "mrkdwn",
-                            "text": f"Demo components currently configured: {options_str}"
-                        }
-                    ]
-                else:
-                    # Display a message when no options are selected
-                    view["blocks"][3]["elements"] = [
-                        {
-                            "type": "mrkdwn",
-                            "text": ":no_entry_sign: Demo components currently configured: *No selections.*"
-                        }
-                    ]
-            else:
-                # Handle case where there are no builder options in the database
-                view["blocks"][3]["elements"] = [
-                    {
-                        "type": "mrkdwn",
-                        "text": ":no_entry_sign: Demo components currently configured: *No selections.*"
-                    }
-                ]
+            #     if display_values:
+            #         # Format the selected options into a string
+            #         options_str = ", ".join(display_values)
+            #         # Update the Block Kit view with the selected options
+            #         view["blocks"][3]["elements"] = [
+            #             {
+            #                 "type": "mrkdwn",
+            #                 "text": f"Demo components currently configured: {options_str}"
+            #             }
+            #         ]
+            #     else:
+            #         # Display a message when no options are selected
+            #         view["blocks"][3]["elements"] = [
+            #             {
+            #                 "type": "mrkdwn",
+            #                 "text": ":no_entry_sign: Demo components currently configured: *No selections.*"
+            #             }
+            #         ]
+            # else:
+            #     # Handle case where there are no builder options in the database
+            #     view["blocks"][3]["elements"] = [
+            #         {
+            #             "type": "mrkdwn",
+            #             "text": ":no_entry_sign: Demo components currently configured: *No selections.*"
+            #         }
+            #     ]
 
             # Publish the updated view to the Slack app home
             client.views_publish(
                 user_id=event["user"],  # User ID from the event
-                view=view
+                view={
+                        "type": "home",
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {
+                                    "type": "mrkdwn",
+                                    "text": "*Welcome home, <@" + event["user"] + "> :house:*"
+                                }
+                            },
+                            {
+                                "type": "section",
+                                "text": {
+                                "type": "mrkdwn",
+                                "text": "Learn how home tabs can be more useful and interactive <https://docs.slack.dev/surfaces/app-home|*in the documentation*>."
+                                }
+                            }
+                        ]
+                    }
             )
             
             # Log the successful update
